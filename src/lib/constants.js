a412784bdc6acb753ab36319d39a0cd3d6eb0b2a
PauSansa <pausansa59@gmail.com>
2023-11-19 19:46:49 +0100

    deploy

---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+export const LINKS = {
+    github: "https://github.com/PauSansa",
+    linkedin: "https://www.linkedin.com/in/PauSansa/",
+    curriculum: "cv/EN_Pau_Sansa_Backend_Developer.pdf"
+  };
+  
